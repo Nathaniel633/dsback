c96e41cd8a8a8f91203d8229c434179ebcb9ecd8
Arushi Pandey <75818107+Arushi-maker@users.noreply.github.com>
2024-05-29 15:14:41 -0700

    accepted changes from head

--- main.py
+++ main.py
@@ -18,15 +18,8 @@ from api.user import user_api # Blueprint import api definition
 from api.player import player_api
 from api.titanic import titanic_api
 from api.fitness import fitness_api
-<<<<<<< HEAD
 from api.CurrentChar import currentchar_api
 from api.CharClass import classes_api
-=======
-from api.stats import stats_api
-from api.CurrentChar import currentchar_api
-from api.CharClass import classes_api
-
->>>>>>> a360eda (main.py change)
 #from api.mental import mental_api
 from api.fitness import sleep_api
 # database migrations
@@ -34,16 +27,10 @@ from model.users import initUsers
 from model.players import initPlayers
 from model.titanicML import initTitanic
 from model.fitnesses import initFitnessModel
-<<<<<<< HEAD
 from model.CurrentChars import initCurrentChars
 from model.classes import initCharClasses
 from model.journal import initMessages
 from model.sleeps import init_sleep
-=======
-from model.journal import initMessages
-from model.sleeps import init_sleep
-from model.statsm import initStats
->>>>>>> a360eda (main.py change)
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -60,14 +47,9 @@ app.register_blueprint(player_api)
 app.register_blueprint(titanic_api) # register api routes
 app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(fitness_api)
-<<<<<<< HEAD
 app.register_blueprint(currentchar_api)
 app.register_blueprint(classes_api)
 app.register_blueprint(sleep_api)
-=======
-app.register_blueprint(sleep_api)
-app.register_blueprint(stats_api)
->>>>>>> a360eda (main.py change)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -92,11 +74,8 @@ def generate_data():
     initPlayers()
     initTitanic()
     initFitnessModel()
-<<<<<<< HEAD
     initCurrentChars()
     initCharClasses()
-=======
->>>>>>> a360eda (main.py change)
     initMessages()
     init_sleep()
     init_stats()
